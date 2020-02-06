ed5f65dc3746b9d90b941faedaae66dfbad12a35
Alex Trainham <alextrainham@gmail.com>
2020-02-06 13:03:27 -0500

    update main application view

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 'use strict'
 
+const hbs = require('hbs'); 
 const express = require('express'); 
 const port = 8080; 
 const path = require('path'); 
 const viewPath = path.join(__dirname, '/src/templates/views'); 
 
+//create express web app
 const app = express(); 
 
 //allow app to recognize css and js files
 app.use(express.static('./src/public')); 
+
+hbs.registerPartials('./src/templates/partials')
 
 app.set('view engine', 'hbs'); 
 app.set('views', viewPath); 
