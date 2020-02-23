59eb240c25f7f512059949496c5b00af4e458f75
Alex Trainham <alextrainham@gmail.com>
2020-02-23 14:47:59 -0500

    refactored for simple

--- src/public/js/objtracker.js
+++ src/public/js/objtracker.js
@@ -1,39 +1,44 @@
-var chosenObj = undefined; 
+var chosenObj = undefined; // for the chosen objective on the page. 
 
-$('#s1').on('click', () => {
-    chosenObj = 's1';
-    $('#difficulty').text('Difficulty: Hard');
-    $('#clue').text("VCUart's theatre where you can catch their latest mainstage production (Be sure to check out their next production coming this April: Spring Awakening).");
-    // Answer: Raymond Hodges Theatre
-    $('#mapclue').attr("src", "/Singleton_Center_Raymond_Hodges_Theatre.png").width("100%");
-    $('#top-right-text').css({"font-weight" : "bold"}).text('Location Area');
-}); 
+const allObj = [{ 
+                    name: 's1', 
+                    diff: 'Hard', 
+                    clue: "VCUart's theatre where you can catch their latest mainstage production (Be sure to check out their next production coming this April: Spring Awakening).",
+                    img: '/Singleton_Center_Raymond_Hodges_Theatre.png'
+                }, 
+                {
+                    name: 's2', 
+                    diff: 'Hard', 
+                    clue: "Hidden park near William Fox Elementary.",
+                    img: '/Singleton_Center_Raymond_Hodges_Theatre.png'
+                },
+                {
+                    name: 's3', 
+                    diff: 'Extra Hard', 
+                    clue: "Richmond Vampire lives here.",
+                    img: '/Singleton_Center_Raymond_Hodges_Theatre.png'
+                },
+                {
+                    name: 's4', 
+                    diff: 'Medium', 
+                    clue: "Two dollar pizza with drunk people!",
+                    img: '/Singleton_Center_Raymond_Hodges_Theatre.png'
+                } 
+];
 
-$('#s2').on('click', () => {
-    chosenObj = 's2';
-    $('#difficulty').text('Difficulty: Hard');
-    $('#clue').text('Hidden park near William Fox Elementary.');
-    $('#mapclue').attr("src", "/Singleton_Center_Raymond_Hodges_Theatre.png").width("100%");
-}); 
-
-$('#s3').on('click', () => {
-    chosenObj = 's3';
-    $('#difficulty').text('Difficulty: Ex Hard');
-    $('#clue').text('Richmond Vampire lives here.');
-    $('#mapclue').attr("src", "/Singleton_Center_Raymond_Hodges_Theatre.png").width("100%");
-}); 
-
-$('#s4').on('click', () => { 
-    chosenObj = 's4';
-    $('#difficulty').text('Difficulty: Medium');
-    $('#clue').text('Two dollar pizza with drunk people!    ');
-    $('#mapclue').attr("src", "/Singleton_Center_Raymond_Hodges_Theatre.png").width("100%");
-}); 
+$.each(allObj, function(i, v) {
+    (function (v) {
+        $("#" + v.name).click(() => {
+            chosenObj = v.name;
+	        $('#difficulty').text(v.diff); 
+	        $('#clue').text(v.clue); 
+	        $('#mapClue').attr("src", v.img).width("100%"); 
+        }); 
+    })(v);  
+});
 
 $('#enter-answer').on('click', () => {
     let answer = $('#answer-box').val();    
-    console.log(answer)
-    console.log(chosenObj)
 
     if(chosenObj === 's2' && answer === 'scuffletown park'){
         $('#isCorrect').text('Correct!');
