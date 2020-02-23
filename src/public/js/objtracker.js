72fe7826137080efaaa1bb2a0585066dfbb95ba4
Noah Weingand <noahw@tahzoo.com>
2020-02-23 16:09:05 -0500

    adding more locations with pictures

--- src/public/js/objtracker.js
+++ src/public/js/objtracker.js
@@ -4,26 +4,62 @@ const allObj = [{
                     name: 's1', 
                     diff: 'Hard', 
                     clue: "VCUart's theatre where you can catch their latest mainstage production (Be sure to check out their next production coming this April: Spring Awakening).",
-                    img: '/Singleton_Center_Raymond_Hodges_Theatre.png'
+                    img: '/spot1.png'
                 }, 
                 {
                     name: 's2', 
                     diff: 'Hard', 
                     clue: "Hidden park near William Fox Elementary.",
-                    img: '/Singleton_Center_Raymond_Hodges_Theatre.png'
+                    img: '/spot2.jpg'
                 },
                 {
                     name: 's3', 
                     diff: 'Extra Hard', 
                     clue: "Richmond Vampire lives here.",
-                    img: '/Singleton_Center_Raymond_Hodges_Theatre.png'
+                    img: '/spot3.jpg'
                 },
                 {
                     name: 's4', 
                     diff: 'Medium', 
                     clue: "Two dollar pizza with drunk people!",
-                    img: '/Singleton_Center_Raymond_Hodges_Theatre.png'
-                } 
+                    img: '/spot4.jpg'
+                },
+                {
+                    name: 's5', 
+                    diff: 'Easy', 
+                    clue: "A place to study with a lingering case of the bed bugs...",
+                    img: '/spot5.jpg'
+                },
+                {
+                    name: 's6', 
+                    diff: 'Easy', 
+                    clue: "Where one would see popular plays, stand up, performers, and homecoming concerts on campus...",
+                    img: '/spot6.jpg'
+                },
+                {
+                    name: 's7', 
+                    diff: 'Easy', 
+                    clue: "Where one will attend ceremonies and graduation...IF you pass your classes...",
+                    img: '/spot7.jpeg'
+                },
+                {
+                    name: 's8', 
+                    diff: 'Easy', 
+                    clue: "Relatively new museum on campus where you can see all mediums of art",
+                    img: '/spot8.jpg'
+                },
+                {
+                    name: 's9', 
+                    diff: 'Medium', 
+                    clue: "Home of VCU's executive overseers who patrol campus",
+                    img: '/spot9.jpg'
+                },
+                {
+                    name: 's10', 
+                    diff: 'Easy', 
+                    clue: "Crowded exercising on campus",
+                    img: '/spot10.jpg'
+                }
 ];
 
 $.each(allObj, function(i, v) {
@@ -32,17 +68,55 @@ $.each(allObj, function(i, v) {
             chosenObj = v.name;
 	        $('#difficulty').text(v.diff); 
 	        $('#clue').text(v.clue); 
-	        $('#mapClue').attr("src", v.img).width("100%"); 
+	        $('#mapclue').attr("src", v.img).width("100%"); 
         }); 
     })(v);  
 });
 
 $('#enter-answer').on('click', () => {
-    let answer = $('#answer-box').val();    
+    let answer = $('#answer-box').val().toLowerCase();    
 
-    if(chosenObj === 's2' && answer === 'scuffletown park'){
+    if(chosenObj === 's1' && answer === 'raymond hodges theatre'){
+        $('#isCorrect').text('Great job!');
+    }
+
+    else if(chosenObj === 's2' && answer === 'scuffletown park'){
         $('#isCorrect').text('Correct!');
-    }else{
+    }
+
+    else if(chosenObj === 's3' && answer === 'hollywood cemetery'){
+        $('#isCorrect').text('Correct!');
+    }
+
+    else if(chosenObj === 's4' && (answer === 'christian\'s pizza' || answer === 'christians pizza')){
+        $('#isCorrect').text('Correct!');
+    }
+
+    else if(chosenObj === 's5' && answer === 'cabell library'){
+        $('#isCorrect').text('Correct!');
+    }
+
+    else if(chosenObj === 's6' && answer === 'altria theater'){
+        $('#isCorrect').text('Correct!');
+    }
+
+    else if(chosenObj === 's7' && answer === 'siegel center'){
+        $('#isCorrect').text('Correct!');
+    }
+
+    else if(chosenObj === 's8' && (answer === 'ica' || answer === 'institute of contemporary art')){
+        $('#isCorrect').text('Correct!');
+    }
+
+    else if(chosenObj === 's9' && answer === 'police station'){
+        $('#isCorrect').text('Correct!');
+    }
+
+    else if(chosenObj === 's10' && (answer === 'cary st gym' || answer === 'cary st. gym' || answer === 'cary street gym')){
+        $('#isCorrect').text('Correct!');
+    }
+    
+    else{
         $('#isCorrect').text('Nope, not quite!');
     }
 });
