93cc7d8f83e381cd3c11472602635c09ababd3c8
Alex Trainham <alextrainham@gmail.com>
2020-04-12 13:42:24 -0400

    mispellings

--- src/public/js/objtracker.js
+++ src/public/js/objtracker.js
@@ -143,7 +143,7 @@ $("#enter-answer").on("click", () => {
   let obj = allObj[index]; 
 
   for(answer of obj.answers){
-      if(answer.toLowerCase() === input_answer.toLowerCase()){
+      if(answer === input_answer){
         $("#isCorrect").text("Correct!");
         element.parentElement.removeChild(element);
         let currentProgress = $("#progress-bar").text();
