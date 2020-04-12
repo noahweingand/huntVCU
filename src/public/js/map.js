3cd793dcce182874d2ac6c1a6b146fb267bcce5a
Alex Trainham <alextrainham@gmail.com>
2020-04-12 12:58:28 -0400

    refactor enter answer code

--- src/public/js/map.js
+++ src/public/js/map.js
@@ -1,72 +1,55 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiY2xhdXJlYSIsImEiOiJjazZpYW9jdXYzMzhoM2VxZjkweDhucWxpIn0.wncNvrarBxvfGydMdy0ZgQ';
-            
-            var map = new mapboxgl.Map({
-                container: 'map', // container id
-                style: 'mapbox://styles/mapbox/streets-v11',
-                center: [-77.45316296360596, 37.548193997957185], // starting position
-                zoom: 14 // starting zoom
-            });
-            
-            // Add geolocate control to the map.
-            // map.addControl(
-            //     new mapboxgl.GeolocateControl({
-            //         positionOptions: {
-            //             enableHighAccuracy: true
-            //         },
-            //         trackUserLocation: true
-            //     })
-            // );
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiY2xhdXJlYSIsImEiOiJjazZpYW9jdXYzMzhoM2VxZjkweDhucWxpIn0.wncNvrarBxvfGydMdy0ZgQ";
 
-          
-            /*
-            map.on('mousemove', function(e) {
-                document.getElementById('info').innerHTML =
-                // e.point is the x, y coordinates of the mousemove event relative
-                // to the top-left corner of the map
-                JSON.stringify(e.point) +
-                '<br />' +
-                // e.lngLat is the longitude, latitude geographical position of the event
-                JSON.stringify(e.lngLat.wrap());
-            });
-            */
-        
-           var radius = 0.00175; 
- 
-           function pointOnCircle(angle, long, lat) {
-           return {
-           'type': 'Point',
-           'coordinates': [(Math.cos(angle) * radius)+long, (Math.sin(angle) * radius) + lat]
-           };
-           }
+var map = new mapboxgl.Map({
+  container: "map", // container id
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [-77.45316296360596, 37.548193997957185], // starting position
+  zoom: 14, // starting zoom
+});
 
-        var longitude = -77.45316296360596
-        var latitude = 37.548193997957185
+var radius = 0.0025; // radius that the circle spins around
 
-map.on('load', function() {
-    map.addSource('point', {
-        'type': 'geojson',
-        'data': pointOnCircle(0)
-        });
-         
-        map.addLayer({
-        'id': 'point',
-        'source': 'point',
-        'type': 'circle',
-        'paint': {
-        'circle-radius': 7,
-        'circle-color': '#FF0000'
-        }
-        });
+function pointOnCircle(angle, long, lat) {
+  return {
+    type: "Point",
+    coordinates: [
+      Math.cos(angle) * radius + long,
+      Math.sin(angle) * radius + lat,
+    ],
+  };
+}
 
-        function animateMarker(timestamp) {
-        // Update the data to a new position based on the animation timestamp. The
-        // divisor in the expression `timestamp / 1000` controls the animation speed.
-        map.getSource('point').setData(pointOnCircle(timestamp /500, longitude, latitude));
-         
-        // Request the next frame of the animation.
-        requestAnimationFrame(animateMarker);
-        }
-         
-        // Start the animation.
-        animateMarker(0);
-})
+var longitude = -77.45316296360596;
+var latitude = 37.548193997957185;
+
+map.on("load", function () {
+  map.addSource("point", {
+    type: "geojson",
+    data: pointOnCircle(0),
+  });
+
+  map.addLayer({
+    id: "point",
+    source: "point",
+    type: "circle",
+    paint: {
+      "circle-radius": 7,
+      "circle-color": "#FF0000",
+    },
+  });
+
+  function animateMarker(timestamp) {
+    // Update the data to a new position based on the animation timestamp. The
+    // divisor in the expression `timestamp / 1000` controls the animation speed.
+    map
+      .getSource("point")
+      .setData(pointOnCircle(timestamp / 500, longitude, latitude));
+
+    // Request the next frame of the animation.
+    requestAnimationFrame(animateMarker);
+  }
+
+  // Start the animation.
+  animateMarker(0);
+});
--- src/public/js/objtracker.js
+++ src/public/js/objtracker.js
@@ -1,4 +1,4 @@
-var chosenObj = undefined; // for the chosen objective on the page. 
+var chosenObj = undefined; // for the chosen objective on the page.
 var check = 0; // Check to set progress bar
 
 // Checks used to make sure each correct answer is only counted once
@@ -13,272 +13,195 @@ var check8 = 0;
 var check9 = 0;
 var check10 = 0;
 
-const allObj = [{ 
-                    name: 's1', 
-                    diff: 'Hard', 
-                    clue: "VCUart's theatre where you can catch their latest mainstage production (Be sure to check out their next production coming this April: Spring Awakening).",
-                    img: '/spot1.png', 
-                    latitude: 37.548534, 
-                    longitude: -77.455252
-                }, 
-                {
-                    name: 's2', 
-                    diff: 'Extra Hard', 
-                    clue: "Old location that sells booze to minors!",
-                    img: '/spot2.jpg',
-                    latitude: 37.550259, 
-                    longitude: -77.447446
-                },
-                {
-                    name: 's3', 
-                    diff: 'Extra Hard', 
-                    clue: "Richmond Vampire lives here.",
-                    img: '/spot3.jpg', 
-                    latitude: 37.539598,  
-                    longitude: -77.454290,
-                },
-                {
-                    name: 's4', 
-                    diff: 'Medium', 
-                    clue: "Two dollar pizza with drunk people!",
-                    img: '/spot4.jpg', 
-                    latitude: 37.550554, 
-                    longitude: -77.453552
-                },
-                {
-                    name: 's5', 
-                    diff: 'Easy', 
-                    clue: "A place to study with a lingering case of the bed bugs...",
-                    img: '/spot5.jpg', 
-                    latitude: 37.547816,
-                    longitude: -77.453203
-                },
-                {
-                    name: 's6', 
-                    diff: 'Easy', 
-                    clue: "Where one would see popular plays, stand up, performers, and homecoming concerts on campus...",
-                    img: '/spot6.jpg', 
-                    latitude: 37.546522,
-                    longitude: -77.451561
-                },
-                {
-                    name: 's7', 
-                    diff: 'Easy', 
-                    clue: "Where one will attend ceremonies and graduation...IF you pass your classes...",
-                    img: '/spot7.jpeg', 
-                    latitude: 37.5525497, 
-                    longitude: -77.4553268
-                },
-                {
-                    name: 's8', 
-                    diff: 'Easy', 
-                    clue: "Relatively new museum on campus where you can see all mediums of art",
-                    img: '/spot8.jpg', 
-                    latitude: 37.548903, 
-                    longitude: -77.448580
-                },
-                {
-                    name: 's9', 
-                    diff: 'Medium', 
-                    clue: "Home of VCU's executive overseers who patrol campus",
-                    img: '/spot9.jpg', 
-                    latitude: 37.544281, 
-                    longitude: -77.4414988
-                },
-                {
-                    name: 's10', 
-                    diff: 'Easy', 
-                    clue: "Crowded exercising on campus",
-                    img: '/spot10.jpg', 
-                    latitude: 37.544457, 
-                    longitude: -77.453989
-                }
+const allObj = [
+  {
+    name: "s1",
+    diff: "Hard",
+    clue:
+      "VCUart's theatre where you can catch their latest mainstage production (Be sure to check out their next production coming this April: Spring Awakening).",
+    img: "/spot1.png",
+    latitude: 37.548534,
+    longitude: -77.455252,
+    answers: ["raymond hodges theatre"]
+  },
+  {
+    name: "s2",
+    diff: "Extra Hard",
+    clue: "Old location that sells booze to minors!",
+    img: "/spot2.jpg",
+    latitude: 37.550259,
+    longitude: -77.447446,
+    answers: ["clay express market", "clay market", "clay street market"]
+  },
+  {
+    name: "s3",
+    diff: "Extra Hard",
+    clue: "Richmond Vampire lives here.",
+    img: "/spot3.jpg",
+    latitude: 37.539598,
+    longitude: -77.45429,
+    answers: ["hollywood cemetary"]
+  },
+  {
+    name: "s4",
+    diff: "Medium",
+    clue: "Two dollar pizza with drunk people!",
+    img: "/spot4.jpg",
+    latitude: 37.550554,
+    longitude: -77.453552,
+    answers: ["christian's pizza", "christians pizza"]
+  },
+  {
+    name: "s5",
+    diff: "Easy",
+    clue: "A place to study with a lingering case of the bed bugs...",
+    img: "/spot5.jpg",
+    latitude: 37.547816,
+    longitude: -77.453203,
+    answers: ["cabell library"]
+  },
+  {
+    name: "s6",
+    diff: "Easy",
+    clue:
+      "Where one would see popular plays, stand up, performers, and homecoming concerts on campus...",
+    img: "/spot6.jpg",
+    latitude: 37.546522,
+    longitude: -77.451561,
+    answers: ["altria theater"]
+  },
+  {
+    name: "s7",
+    diff: "Easy",
+    clue:
+      "Where one will attend ceremonies and graduation...IF you pass your classes...",
+    img: "/spot7.jpeg",
+    latitude: 37.5525497,
+    longitude: -77.4553268,
+    answers: ["siegel center"]
+  },
+  {
+    name: "s8",
+    diff: "Easy",
+    clue:
+      "Relatively new museum on campus where you can see all mediums of art",
+    img: "/spot8.jpg",
+    latitude: 37.548903,
+    longitude: -77.44858,
+    answers: ["institute of contemporary art"]
+  },
+  {
+    name: "s9",
+    diff: "Medium",
+    clue: "Home of VCU's executive overseers who patrol campus",
+    img: "/spot9.jpg",
+    latitude: 37.544281,
+    longitude: -77.4414988,
+    answers: ["police station"]
+  },
+  {
+    name: "s10",
+    diff: "Easy",
+    clue: "Crowded exercising on campus",
+    img: "/spot10.jpg",
+    latitude: 37.544457,
+    longitude: -77.453989,
+    answers: ["cary st gym", "cary st. gym", "cary street gym"]
+  },
 ];
 
-$.each(allObj, function(i, v) {
-    (function (v) {
-        $("#" + v.name).click(() => {
-            if(check < 1){
-                let currentProgress = $('#progress-bar').text();
-                checkProgress(currentProgress);
-                check = check + 1;
-            }
-            chosenObj = v.name;
-            if(chosenObj !== undefined){
-                $('#top-right-text').text('Image Clue:'); 
-                $('#top-right-text').css('font-weight', 'Bold')
-            }
-	        $('#difficulty').text("Difficulty: " + v.diff); 
-	        $('#clue').text(v.clue); 
-          $('#mapclue').attr("src", v.img).width("100%"); 
-          longitude = v.longitude; 
-          latitude = v.latitude; 
-          map.flyTo({center: [longitude, latitude], zoom: 15.5})
-        }); 
-    })(v);  
+// onclick method for each spot objective anchor thing
+$.each(allObj, function (i, v) {
+  (function (v) {
+    $("#" + v.name).click(() => {
+      if (check < 1) {
+        let currentProgress = $("#progress-bar").text();
+        checkProgress(currentProgress);
+        check = check + 1;
+      }
+      chosenObj = v.name;
+      if (chosenObj !== undefined) {
+        $("#top-right-text").text("Image Clue:");
+        $("#top-right-text").css("font-weight", "Bold");
+      }
+      $("#difficulty").text("Difficulty: " + v.diff);
+      $("#clue").text(v.clue);
+      $("#mapclue").attr("src", v.img).width("100%");
+      longitude = v.longitude;
+      latitude = v.latitude;
+      map.flyTo({ center: [longitude, latitude], zoom: 15.5 });
+    });
+  })(v);
 });
 
-$('#enter-answer').on('click', () => {
-    let answer = $('#answer-box').val().toLowerCase();    
+$("#enter-answer").on("click", () => {
+  let input_answer = $("#answer-box").val().toLowerCase();
+  let element = document.querySelector('#'+chosenObj); 
 
-    if(chosenObj === 's1' && answer === 'raymond hodges theatre'){
-        $('#isCorrect').text('Correct!');
-        if(check1 < 1){
-            let currentProgress = $('#progress-bar').text();
-            checkProgress(currentProgress);
-            check1 = check1 + 1;
-        }
-    }
+  let index = parseInt(chosenObj[1]) - 1; 
+  let obj = allObj[index]; 
 
-    else if(chosenObj === 's2' && (answer === 'clay express market' || answer === 'clay market' || answer === 'clay street market')){
-        $('#isCorrect').text('Correct!');
-        if(check2 < 1){
-            let currentProgress = $('#progress-bar').text();
-            checkProgress(currentProgress);
-            check2 = check2 + 1;
-        }
-    }
-
-    else if(chosenObj === 's3' && answer === 'hollywood cemetery'){
-        $('#isCorrect').text('Correct!');
-        if(check3 < 1){
-            let currentProgress = $('#progress-bar').text();
-            checkProgress(currentProgress);
-            check3 = check3 + 1;
-        }
-    }
-
-    else if(chosenObj === 's4' && (answer === 'christian\'s pizza' || answer === 'christians pizza')){
-        $('#isCorrect').text('Correct!');
-        if(check4 < 1){
-            let currentProgress = $('#progress-bar').text();
-            checkProgress(currentProgress);
-            check4 = check4 + 1;
-        }
-    }
-
-    else if(chosenObj === 's5' && answer === 'cabell library'){
-        $('#isCorrect').text('Correct!');
-        if(check5 < 1){
-            let currentProgress = $('#progress-bar').text();
-            checkProgress(currentProgress);
-            check5 = check5 + 1;
-        }
-    }
-
-    else if(chosenObj === 's6' && answer === 'altria theater'){
-        $('#isCorrect').text('Correct!');
-        if(check6 < 1){
-            let currentProgress = $('#progress-bar').text();
-            checkProgress(currentProgress);
-            check6 = check6 + 1;
-        }
-    }
-
-    else if(chosenObj === 's7' && answer === 'siegel center'){
-        $('#isCorrect').text('Correct!');
-        if(check7 < 1){
-            let currentProgress = $('#progress-bar').text();
-            checkProgress(currentProgress);
-            check7 = check7 + 1;
-        }
-    }
-
-    else if(chosenObj === 's8' && (answer === 'ica' || answer === 'institute of contemporary art')){
-        $('#isCorrect').text('Correct!');
-        if(check8 < 1){
-            let currentProgress = $('#progress-bar').text();
-            checkProgress(currentProgress);
-            check8 = check8 + 1;
-        }
-    }
-
-    else if(chosenObj === 's9' && answer === 'police station'){
-        $('#isCorrect').text('Correct!');
-        if(check9 < 1){
-            let currentProgress = $('#progress-bar').text();
-            checkProgress(currentProgress);
-            check9 = check9 + 1;
-        }
-    }
-
-    else if(chosenObj === 's10' && (answer === 'cary st gym' || answer === 'cary st. gym' || answer === 'cary street gym')){
-        $('#isCorrect').text('Correct!');
-        if(check10 < 1){
-            let currentProgress = $('#progress-bar').text();
-            checkProgress(currentProgress);
-            check10 = check10 + 1;
-        }
-    }
-    
-    else{
-        $('#isCorrect').text('Nope, not quite!');
-    }
+  for(answer of obj.answers){
+      if(answer === input_answer.toLowerCase()){
+        $("#isCorrect").text("Correct!");
+        element.parentElement.removeChild(element);
+        let currentProgress = $("#progress-bar").text();
+        checkProgress(currentProgress);
+        return; 
+      }
+  }
+  $("#isCorrect").text("Nope, not quite!");
 });
 
 function checkProgress(percentage) {
-
-    if(percentage === '0%'){
-        $('.progress-bar').css('width', '1%');
-        $('.progress-bar').css('background-color', '#FF0000');
-        $('#progress-bar-text').text('1%');
-    }
-    else if(percentage === '1%'){
-        $('.progress-bar').css('width', '10%');
-        $('.progress-bar').css('background-color', '#FF3300');
-        $('#progress-bar-text').text('10%');       
-    }
-    else if(percentage === '10%'){
-        $('#twenty').click();
-        $('.progress-bar').css('width', '20%');
-        $('.progress-bar').css('background-color', '#FF6600');
-        $('#progress-bar-text').text('20%');     
-    }
-    else if(percentage === '20%'){
-        $('.progress-bar').css('width', '30%');
-        $('.progress-bar').css('background-color', '#FF9900');
-        $('#progress-bar-text').text('30%');     
-    }
-    else if(percentage === '30%'){
-        $('.progress-bar').css('width', '40%');
-        $('.progress-bar').css('background-color', '#FFCC00');
-        $('#progress-bar-text').text('40%');  
-    }
-    else if(percentage === '40%'){
-        $('.progress-bar').css('width', '50%');
-        $('.progress-bar').css('background-color', '#FFFF00');
-        $('#progress-bar-text').text('50%');     
-    }
-    else if(percentage === '50%'){
-        $('.progress-bar').css('width', '60%');
-        $('.progress-bar').css('background-color', '#CCFF00');
-        $('#progress-bar-text').text('60%');     
-    }
-    else if(percentage === '60%'){
-        $('.progress-bar').css('width', '70%');
-        $('.progress-bar').css('background-color', '#99FF00');
-        $('#progress-bar-text').text('70%');     
-    }
-    else if(percentage === '70%'){
-        $('.progress-bar').css('width', '80%');
-        $('.progress-bar').css('background-color', '#66FF00');
-        $('#progress-bar-text').text('80%');     
-    }
-    else if(percentage === '80%'){
-        $('.progress-bar').css('width', '90%');
-        $('.progress-bar').css('background-color', '#33FF00');
-        $('#progress-bar-text').text('90%');     
-    }
-    else if(percentage === '90%'){
-        $('.progress-bar').css('width', '100%');
-        $('.progress-bar').css('background-color', '#00FF00');
-        $('#progress-bar-text').text('100%');     
-    }
-    else if(percentage === '100%'){
-        ;    
-    }
-    else{
-        $('.progress-bar').css('width', '0%');
-        $('#progress-bar-text').text('ERROR!');
-    }
+  if (percentage === "0%") {
+    $(".progress-bar").css("width", "1%");
+    $(".progress-bar").css("background-color", "#FF0000");
+    $("#progress-bar-text").text("1%");
+  } else if (percentage === "1%") {
+    $(".progress-bar").css("width", "10%");
+    $(".progress-bar").css("background-color", "#FF3300");
+    $("#progress-bar-text").text("10%");
+  } else if (percentage === "10%") {
+    $("#twenty").click();
+    $(".progress-bar").css("width", "20%");
+    $(".progress-bar").css("background-color", "#FF6600");
+    $("#progress-bar-text").text("20%");
+  } else if (percentage === "20%") {
+    $(".progress-bar").css("width", "30%");
+    $(".progress-bar").css("background-color", "#FF9900");
+    $("#progress-bar-text").text("30%");
+  } else if (percentage === "30%") {
+    $(".progress-bar").css("width", "40%");
+    $(".progress-bar").css("background-color", "#FFCC00");
+    $("#progress-bar-text").text("40%");
+  } else if (percentage === "40%") {
+    $(".progress-bar").css("width", "50%");
+    $(".progress-bar").css("background-color", "#FFFF00");
+    $("#progress-bar-text").text("50%");
+  } else if (percentage === "50%") {
+    $(".progress-bar").css("width", "60%");
+    $(".progress-bar").css("background-color", "#CCFF00");
+    $("#progress-bar-text").text("60%");
+  } else if (percentage === "60%") {
+    $(".progress-bar").css("width", "70%");
+    $(".progress-bar").css("background-color", "#99FF00");
+    $("#progress-bar-text").text("70%");
+  } else if (percentage === "70%") {
+    $(".progress-bar").css("width", "80%");
+    $(".progress-bar").css("background-color", "#66FF00");
+    $("#progress-bar-text").text("80%");
+  } else if (percentage === "80%") {
+    $(".progress-bar").css("width", "90%");
+    $(".progress-bar").css("background-color", "#33FF00");
+    $("#progress-bar-text").text("90%");
+  } else if (percentage === "90%") {
+    $(".progress-bar").css("width", "100%");
+    $(".progress-bar").css("background-color", "#00FF00");
+    $("#progress-bar-text").text("100%");
+  } else if (percentage === "100%") {
+  } else {
+    $(".progress-bar").css("width", "0%");
+    $("#progress-bar-text").text("ERROR!");
+  }
 }
